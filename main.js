3f92aa6ad4f21f4c5b07038a074c231ce5712b40
Ervin <evgnz24@gmail.com>
2023-04-10 13:22:31 -0500

    Completed Knight Traversal function, Also added some base cases for handling only one input, as well as ranges outside of the board

--- main.js
+++ main.js
@@ -31,20 +31,75 @@ class Game {
     return this.board;
   }
 
-  knightTraversal([startX, startY], [endX, endY]) {
-    if (
-      startX < 0 ||
-      startY < 0 ||
-      startX > this.boardLength ||
-      startY > this.boardLength
-    )
-      return `Please choose a valid position. Between (0,0) - (${
-        this.boardLength - 1
-      },${this.boardLength - 1})`;
+  showMoves(moves) {
+    console.log(
+      `Congratulations you made it in ${
+        moves.length - 1
+      } moves!  Here is your path`
+    );
+    moves.forEach((move) => {
+      console.log(move);
+    });
+  }
+
+  knightTraversal(start, end) {
+    let level = 0;
+    let queue = [start];
+    const visited = new Set(queue);
+    const distance = { [start]: { distance: 0, path: [start] } };
+
+    // go while queue is not empty
+    while (queue.length > 0) {
+      // Base Case
+      if (
+        start[0] < 0 ||
+        start[1] < 0 ||
+        end[0] < 0 ||
+        end[1] < 0 ||
+        start[0] > this.board.length - 1 ||
+        start[1] > this.board.length - 1 ||
+        end[0] > this.board.length - 1 ||
+        end[1] > this.board.length - 1
+      )
+        return console.log('Not a valid location');
+
+      // Base case if only one x or y
+      if (start.length !== 2) return console.log('Check your start point');
+      if (end.length !== 2) return console.log('Check your end point');
+
+      //get first element
+      const currentPosition = queue.shift();
+
+      // Check if current matches
+      if (currentPosition[0] === end[0] && currentPosition[1] === end[1]) {
+        // return `Found [${currentPosition}] in level ${level}`;
+        return this.showMoves(distance[currentPosition].path);
+      }
+
+      // Increase level
+      level++;
+
+      const neighbors = this.neighbors(currentPosition[0], currentPosition[1]);
+
+      // check if neighbor has been visited
+      const unvisitedNeighbors = neighbors.filter(
+        (neighbor) => !visited.has(neighbor.toString())
+      );
+
+      //add neighbors to queue
+      unvisitedNeighbors.forEach((neighbor) => {
+        queue.push(neighbor);
+        visited.has(neighbor.toString());
+        distance[neighbor] = {
+          distance: level,
+          path: [...distance[currentPosition].path, neighbor],
+        };
+      });
+    }
   }
 
   // Gets all the neighbors of a point
-  adjecencyList(xCoord, yCoord) {
+  neighbors(xCoord, yCoord) {
     const moves = [
       [-2, -1],
       [-2, 1],
@@ -64,7 +119,7 @@ class Game {
     for (let i = 0; i < moves.length; i++) {
       const move = moves[i];
       const newX = xCoord + move[0];
-      const newY = xCoord + move[1];
+      const newY = yCoord + move[1];
 
       //check if move is valid
       if (
@@ -74,7 +129,11 @@ class Game {
         newY < this.boardLength &&
         !this.board[newX][newY].visited
       ) {
-        movesForSquare.push(newX, newY);
+        // Prevent repetitive moves
+        this.board[newX][newY].visited = true;
+
+        // Move valid move to new array
+        movesForSquare.push([newX, newY]);
       }
     }
 
@@ -85,7 +144,10 @@ class Game {
 const game = new Game();
 
 // console.log(game.board);
-// console.log(game.adjecencyList(4, 5));
-// console.log(game.adjecencyList(5, 7));
-// console.log(game.adjecencyList(0, 5));
-console.log(game.adjecencyList(0, 5));
+// console.log(game.neighbors(4, 5));
+// console.log(game.neighbors(5, 7));
+// console.log(game.neighbors(0, 5));
+// console.log(game.knightTraversal([3, 3], [4, 3]));
+// game.knightTraversal([39, 5], [7, 0]); // Throws errors, out of range
+// game.knightTraversal([7, 5], [0, 3]);
+game.knightTraversal([7, 7], [0, 0]);
